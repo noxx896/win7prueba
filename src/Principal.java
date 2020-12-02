9fe494c72adb80f59b8539f7e0c8471b05e55893
n0xx <smc.8645@gmail.com>
2020-12-02 12:29:51 -0500

    Initial commit

---
+++ src/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dop
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello mofo");
+        System.out.println("how are you?");
+    }
+    
+}
